4b79026216ad2680236a0c1abbd6146350b7d1d1
Leo Gavin <leogavin123@outlook.com>
2024-06-09 11:57:45 +0530

    Portfolio on bussing

---
+++ script.lua
@@ -0,0 +1,11 @@
+local contactEmail = get("a")[1]
+contactEmail.set_href("mailto:leogavin123@outlook.com")
+contactEmail.set_contents("leogavin123@outlook.com")
+
+local linkedin = get("a")[2]
+linkedin.set_href("https://www.linkedin.com/in/leogavin/")
+linkedin.set_contents("Leo Gavin")
+
+local github = get("a")[3]
+github.set_href("https://github.com/DFanso")
+github.set_contents("github.com/DFanso")
